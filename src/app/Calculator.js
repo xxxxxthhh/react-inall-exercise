729509b1ead4516576dd003051869e1a9cef2826
KyleXu <kyle_x@foxmail.com>
2020-09-25 02:01:46 +0800

    [XTH] finished calculator & add less for home page :>

--- src/app/Calculator.js
+++ src/app/Calculator.js
@@ -1,13 +1,103 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-class Calculator extends Component{
+class Calculator extends Component {
+  state = {
+    formula: "",
+  };
+
+  handleInput = (e) => {
+    this.setState({
+      formula: this.state.formula + String(e.target.value),
+    });
+  };
+
+  handleReset = () => {
+    this.setState({
+      formula: "",
+    });
+  };
+
+  handleCalculate = () => {
+    const regex = /\d+[\+\-\*]\d+/;
+    console.log(this.state.formula.replace(regex, "").length);
+    console.log(this.state.formula);
+    if (this.state.formula.replace(regex, "").length !== 0) {
+      this.handleReset();
+    } else {
+      console.log(this.state.formula);
+      this.setState({
+        formula: eval(this.state.formula),
+      });
+    }
+  };
+
   render() {
     return (
-      <div>
-        <h1>Hello</h1>
+      <div className="calculator">
+        <h1>在线计算器</h1>
+        <div className="calPadSround">
+          <input
+            className="screen"
+            type="text"
+            readOnly={true}
+            value={this.state.formula}
+          />
+          <div className="calPad">
+            <button id="add" value="+" onClick={this.handleInput}>
+              +
+            </button>
+            <button id="minus" value="-" onClick={this.handleInput}>
+              -
+            </button>
+            <button id="multiply" value="*" onClick={this.handleInput}>
+              X
+            </button>
+
+            <button id="one" value="1" onClick={this.handleInput}>
+              1
+            </button>
+            <button id="two" value="2" onClick={this.handleInput}>
+              2
+            </button>
+            <button id="three" value="3" onClick={this.handleInput}>
+              3
+            </button>
+            <button id="four" value="4" onClick={this.handleInput}>
+              4
+            </button>
+            <button id="five" value="5" onClick={this.handleInput}>
+              5
+            </button>
+            <button id="six" value="6" onClick={this.handleInput}>
+              6
+            </button>
+            <button id="seven" value="7" onClick={this.handleInput}>
+              7
+            </button>
+            <button id="eight" value="8" onClick={this.handleInput}>
+              8
+            </button>
+            <button id="nine" value="9" onClick={this.handleInput}>
+              9
+            </button>
+            <button id="zero" value="0" onClick={this.handleInput}>
+              0
+            </button>
+            <button id="reset" value="c" onClick={this.handleReset}>
+              Clear
+            </button>
+            <button id="equals" value="=" onClick={this.handleCalculate}>
+              =
+            </button>
+          </div>
+        </div>
+
+        <Link className="homeLinkInCal" to="/">
+          回到主页
+        </Link>
       </div>
-    )
+    );
   }
 }
-
 export default Calculator;
--- src/app/Home.js
+++ src/app/Home.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import './home.less';
+import React from "react";
+import "./home.less";
+import backGroundURL from "../images/hero-image.png";
+import calculatorURL from "../images/calculator.png";
+import timerURL from "../images/timer.png";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return (<div className="home">
-    This is home page
-  </div>);
+  return (
+    <div className="home">
+      <div className="homeAbove">
+        <h1>在线实用工具</h1>
+        <img
+          className="backGroundImg"
+          src={backGroundURL}
+          alt="backGroundURL"
+        ></img>
+      </div>
+
+      <div className="homeBelow">
+        <div className="homePageIcon">
+          <img
+            className="calculatorImg"
+            src={calculatorURL}
+            alt="calculatorURL"
+          ></img>
+          <img className="timerImg" src={timerURL} alt="timerURL"></img>
+        </div>
+        <div className="linkBelowIcon">
+          <Link className="countDowntLinktimerIcon" to="/calculator">
+            计算器
+          </Link>
+          <Link className="calculatorLinktimerIcon" to="/timer">
+            倒计时器
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
