0871d67bb92558aff3b454012e77f0eed241ead3
KyleXu <kyle_x@foxmail.com>
2020-09-25 00:52:14 +0800

    [XTH] add calculator & timer & NotMatch

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,33 @@
-import React, {Component} from 'react';
-import './app.less';
-import {Route, BrowserRouter} from "react-router-dom";
+import React, { Component } from "react";
+import "./app.less";
+import { Route, BrowserRouter, Link, Switch } from "react-router-dom";
 import Home from "./Home";
+import Calculator from "./Calculator";
+import Timer from "./Timer";
+import NotMatch from "./NotMatch";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <BrowserRouter>
-          <Route exact path='/' component={Home}/>
+          <div className="header">
+            <Link className="calculatorLink" to="/calculator">
+              Calculator
+            </Link>
+            <Link className="timerLink" to="/timer">
+              Timer
+            </Link>
+            <Link className="homeLink" to="/">
+              Home
+            </Link>
+          </div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/calculator" component={Calculator} />
+            <Route exact path="/timer" component={Timer} />
+            <Route component={NotMatch} />
+          </Switch>
         </BrowserRouter>
       </div>
     );
---
+++ src/app/Timer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Timer extends Component{
+  render() {
+    return (
+      <div>
+        <h1>Hi</h1>
+      </div>
+    )
+  }
+}
+
+export default Timer;
